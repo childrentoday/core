d560550e29b3e2a04e2a1bc82851577adc21af5b
childrentoday <baronovskidit@mail.ru>
2020-05-21 23:32:16 +0500

    Update connect_db.php

--- connect_db.php
+++ connect_db.php
@@ -1,14 +1,17 @@
 <?php
-//подключим файл с настройками
+//подключаем файл с настройками
 include('config.php');
 
-// стыковка с сервером 
+// выполним стыковку с сервером 
 $connect = mysqli_connect($server, $user, $pass);
 if(!$connect) {
 	print("Ошибка соединения с сервером".mysqli_connect_error());
 	print("<br>Код ошибки:".mysqli_connect_errno());
 	exit();
 } /* else print("Есть коннект"); */
+
 // выбираем с какой базой будем работать
 mysqli_select_db($connect, $db);
+
+// устанавливаем кодировку для базы
 mysqli_set_charset($connect, $utf);
