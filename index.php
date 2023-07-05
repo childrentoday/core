26204911984f50a9cfdcde21ffe9a5eff259aaf3
childrentoday <baronovskidit@mail.ru>
2023-07-05 20:52:22 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php 
 /*-----01-----*/
-//---вставка /template/inc.php
+//---вставка  /template/inc.php
 //---описание /readme/inc.md
-require_once $_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR .'template'. DIRECTORY_SEPARATOR . 'config.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR .'template'. DIRECTORY_SEPARATOR . 'inc.php';
