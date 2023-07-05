77bf2e9c44f612ba6578b4b3d00e4ca6a311fb8d
childrentoday <baronovskidit@mail.ru>
2023-07-05 20:47:03 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php 
+/*-----01-----*/
+//---вставка /template/inc.php
+//---описание /readme/inc.md
+require_once $_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR .'template'. DIRECTORY_SEPARATOR . 'config.php';
